1e700440068fb3add60b24d91eed445dbeed7ccd
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2018-01-04 16:43:48 +0530

    Update aws_codedeploy_config.sh

--- awsscripts/aws_codedeploy_config.sh
+++ awsscripts/aws_codedeploy_config.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-mkdir -p /var/codedeploy/nginx-sample
+mkdir -p /var/codedeploy/tomcat2-sample
 
-cat <<EOF >/var/codedeploy/nginx-sample/env.properties
+cat <<EOF >/var/codedeploy/tomcat2-sample/env.properties
 APPLICATION_NAME=$APPLICATION_NAME
 DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
 DEPLOYMENT_ID=$DEPLOYMENT_ID
